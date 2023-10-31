110e27d3e95b052794d17c17d6287945c41f88a7
kaxa-bekov <kaxa.bekov@gmail.com>
2023-10-31 06:14:01 -0400

    Added documentation and comments

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+// Getting all of the needed elements using DOM references
+
 const descriptionInput = document.getElementById("description-input");
 const deadlineInput = document.getElementById("deadline-input");
 const addButton = document.getElementById("add");
-const taskTable = document.getElementById("task-table");
 const removeButton = document.getElementById("remove");
 const taskTableBody = document.getElementById("task-table-body");
 const checkAllCheckbox = document.getElementById('check-all');
 
+//Preparing an empty variable for the table rows collection
 let tableRowsCollection = null;
 
-
+// This function creates a new collection of all the current rows of the table and returns it
 function updateRowsCollection(){
     tableRowsCollection = taskTableBody.rows;
     return tableRowsCollection;
 }
 
+// Add Button Event Listener. It checks that the input value is not empty, then creates 3 new table datat elements adn initializes them based on the input and deadline values.
+// Also adds a checkbox input on to the first cell of each row.
+// Then it appends all of those table datas toa newly created table row element, which then gets appended to the table body element.
+// After that we can reset the inputs and keep the input field focused and finally call the updateRowsCollection() function to create and return a list of rows.
 addButton.addEventListener('click', () => {
     console.log("item added");
 
@@ -50,6 +56,10 @@ addButton.addEventListener('click', () => {
     
 })
 
+
+// This button checks if we selected anything using the checkboxes in the first cell of each row, then iterates BACKWARDS on the rows collection and creates a cells collection on each row.
+// THen checks if each rows, first cell's checkbox is checked it removes that entire row for the table body.
+//Finally it resets all the checkboxes to unchecked and updates the rows collection
 removeButton.addEventListener('click', () =>{
 
     try{
@@ -69,6 +79,10 @@ removeButton.addEventListener('click', () =>{
     }
 })
 
+
+// The headesr first cell has a checkbox for selecting all of the table rows.
+// It is implemented here by converting the rows collection into an array and itrerationg using forEach() function where we set the checkboxes to checked. 
+// In case if there are no rows in the table we show an alert message and set the main checkbox to uncheched as well as if there is an error.
 checkAllCheckbox.addEventListener('change', () =>{
 
 try{
